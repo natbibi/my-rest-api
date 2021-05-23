4f62199f1a292f9f253594406f2f7330b2850f5b
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-23 15:49:28 +0100

    :seedling: create model, migrate and add a seed

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Destination(models.Model):
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    image = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.city
