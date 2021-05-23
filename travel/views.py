b22a2ace1b8130a9a669250023814eee9d8c511b
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-23 16:16:00 +0100

    :truck: add CRUD for Destinations

--- travel/views.py
+++ travel/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Destination
+from .serializers import DestinationSerializer
 
-# Create your views here.
+# GET, POST
+class DestinationList(generics.ListCreateAPIView):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
+
+# PATCH, DELETE
+class DestinationDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Destination.objects.all()
+    serializer_class = DestinationSerializer
