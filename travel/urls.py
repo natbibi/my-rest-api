b2d13e6136ab9186c662e032ed72269951193645
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-23 16:17:16 +0100

    :truck: add path /destinations

---
+++ travel/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('destinations/', views.DestinationList.as_view()),
+    path('destinations/<int:pk>', views.DestinationDetail.as_view())
+]
