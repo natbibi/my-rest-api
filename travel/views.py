b64b4f48842048a02d1535e7222211259e596c02
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-23 16:28:35 +0100

    :passport_control: add auth so paths are read only unless authed

--- travel/views.py
+++ travel/views.py
@@ -1,13 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Destination
 from .serializers import DestinationSerializer
 
 # GET, POST
 class DestinationList(generics.ListCreateAPIView):
     queryset = Destination.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = DestinationSerializer
 
 # PATCH, DELETE
 class DestinationDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Destination.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = DestinationSerializer
