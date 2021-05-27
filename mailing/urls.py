0195e7c6ff5b2ffdc782438eb83eb3d8981eade3
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-27 23:00:59 +0100

    :pencil2: change to MailingDetail in url

--- mailing/urls.py
+++ mailing/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('mailinglist/', views.MailingList.as_view()),
-    path('mailinglist/<int:pk>', views.MailingList.as_view())
+    path('mailinglist/<int:pk>', views.MailingDetail.as_view())
 ]
