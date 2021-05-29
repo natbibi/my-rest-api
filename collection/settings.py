ed66f9a2b24acebacd2d642d3aaafc869720d9a8
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-29 23:14:17 +0100

    :wrench: allow read access for AWS files

--- collection/settings.py
+++ collection/settings.py
@@ -162,6 +162,7 @@ AWS_SECRET_ACCESS_KEY = env.str('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME ='ste-food-blog'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
+AWS_DEFAULT_ACL = 'public-read'
 
 AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
 AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
