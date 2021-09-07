dc80be8ae1ef3722fbf745710d0b642a81cc1ad3
natbibi <natalie.wuwu@googlemail.com>
2021-09-07 18:42:47 +0100

    :ambulance: add sorting by id to /projects

--- projects/views.py
+++ projects/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics, permissions
 from .models import Project
 from .serializers import ProjectSerializer
 from drf_spectacular.utils import extend_schema
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+
 
 # GET, POST
 class ProjectList(generics.ListCreateAPIView):
@@ -11,6 +14,9 @@ class ProjectList(generics.ListCreateAPIView):
     queryset = Project.objects.all()
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = ProjectSerializer
+    filter_backends = (DjangoFilterBackend, OrderingFilter)
+    ordering_fields = ['id']
+    ordering = ['id']
 
 # PATCH, DELETE
 class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
